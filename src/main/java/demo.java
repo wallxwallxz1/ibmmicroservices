f96036b1498233a9f1225523fbbc8913847f3e38
wall wall <wallxwallxz1@gmail.com>
2019-12-22 04:24:50 -0600

    IBM MicroClimate Demo

---
+++ src/main/java/demo.java
@@ -0,0 +1,9 @@
+public class demo {
+
+    public static void main(String [] args)
+    {
+        System.out.println("[ some data ] \n");
+
+        System.out.println("[ some  more data ]");
+    }
+}
